c3bd3334a61bce7d17e8a078bd34492ff3aeb1e4
(minjae8563) <(minjae8563@naver.com)>
2025-06-15 17:34:36 +0900

    feat: @Configuration 애너테이션 추가

--- src/main/java/finalmission/global/config/WebConfig.java
+++ src/main/java/finalmission/global/config/WebConfig.java
@@ -2,9 +2,11 @@ package finalmission.global.config;
 
 import finalmission.customer.resolver.LoginCustomerArgumentResolver;
 import java.util.List;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCustomerArgumentResolver loginCustomerArgumentResolver;
